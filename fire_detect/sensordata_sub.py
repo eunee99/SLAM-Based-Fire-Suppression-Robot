21fe6ae0bd151a9d4deb7188ec044bc4dec8b7b1
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2022-06-27 19:10:09 +0900

    sensor data_sub

---
+++ fire_detect/sensordata_sub.py
@@ -0,0 +1,14 @@
+import paho.mqtt.client as mqtt
+server = "13.125.77.88"
+def on_connect(client, userdata, flags, rc):
+    print("Connected with RC : " + str(rc))
+    client.subscribe("#")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+msg.payload.decode('utf-8'))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(server, 1883, 60)
+client.loop_forever()
