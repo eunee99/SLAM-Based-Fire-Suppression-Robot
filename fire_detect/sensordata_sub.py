9f9b975bca9befaf3dad65afa4648b338b34534d
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2022-06-28 17:38:24 +0900

    Update sensordata_sub.py

--- fire_detect/sensordata_sub.py
+++ fire_detect/sensordata_sub.py
@@ -1,14 +1,19 @@
 import paho.mqtt.client as mqtt
 server = "13.125.77.88"
-
-#server connect
 def on_connect(client, userdata, flags, rc):
     print("Connected with RC : " + str(rc))
     client.subscribe("#")
 
-#message  
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+msg.payload.decode('utf-8'))
+    if(msg.topic == "deviceid/juju/evt/flame"):
+        flamevalue = float(msg.payload.decode('utf-8'))
+        print(msg.topic+" "+msg.payload.decode('utf-8'))
+        if(flamevalue == 1):
+            print("화재 경보")
+            client.publish("deviceid/juju/cmd/flame", "1") # 화재감지기로부터 값을 받아오면 robot에게 publish
+        else:
+            print("안전")
+            client.publish("deviceid/juju/cmd/flame", "0")
 
 client = mqtt.Client()
 client.on_connect = on_connect
